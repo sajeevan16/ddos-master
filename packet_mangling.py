5084f4401ace60766d02b10e940a97647c137d01
sajevan16 <sajeevan.16@cse.mrt.ac.lk>
2020-12-13 10:56:01 +0530

    update packet mangling

--- packet_mangling.py
+++ packet_mangling.py
@@ -1,20 +1,41 @@
-import nfqueue
+# import nfqueue
 import socket
 from dpkt import ip
 
 
-q = None
-def cb(dummy, payload):
-    # make decision about if the packet should be allowed. in this case, drop everything:
-    payload.set_verdict(nfqueue.NF_DROP)
-q = nfqueue.queue()
-q.open()
-q.bind(socket.AF_INET)#q.bind()
-q.set_callback(cb)
-q.create_queue(0) 
+# q = None
+# def cb(dummy, payload):
+#     # make decision about if the packet should be allowed. in this case, drop everything:
+#     payload.set_verdict(nfqueue.NF_DROP)
+# q = nfqueue.queue()
+# q.open()
+# q.bind(socket.AF_INET)#q.bind()
+# q.set_callback(cb)
+# q.create_queue(0) 
+# try:
+#         q.try_run()
+# except KeyboardInterrupt:
+#         print ("Exiting...")
+# q.unbind(socket.AF_INET)
+# q.close()
+
+
+from netfilterqueue import NetfilterQueue
+import socket
+
+def print_and_accept(pkt):
+    print(pkt)
+    pkt.accept()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(1, print_and_accept)
+s = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+
 try:
-        q.try_run()
+    nfqueue.run_socket(s)
 except KeyboardInterrupt:
-        print ("Exiting...")
-q.unbind(socket.AF_INET)
-q.close()
+    print('KeyboardInterrupt')
+
+s.close()
+nfqueue.unbind()
+
